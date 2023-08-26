61884f5925a93dcf77c17cda16609b54de0832a3
zgweks <92344996+zgwek@users.noreply.github.com>
2023-08-26 14:06:33 -0400

    add index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,37 +10,8 @@ export {
   ErrorBoundary,
 } from 'expo-router';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
-}
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
@@ -48,9 +19,9 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="list" options={{ title: 'My Grocery List' }} />
       </Stack>
     </ThemeProvider>
   );
 }
+export default RootLayoutNav;
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Redirect } from 'expo-router';
+
+const index = () => {
+  return <Redirect href="/list"/>;
+  
+};
+
+export default index;
